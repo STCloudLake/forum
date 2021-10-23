fdb232980707dcc7d9bbeedd5a45a2977c58bb94
Xther <1146855447@qq.com>
2021-10-23 19:17:08 +0800

    sfsfsf

---
+++ forum-web/src/main/java/com/join/ForumWebApplication.java
@@ -0,0 +1,16 @@
+package com.join;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hx焘
+ */
+@SpringBootApplication
+public class ForumWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumWebApplication.class, args);
+    }
+
+}
