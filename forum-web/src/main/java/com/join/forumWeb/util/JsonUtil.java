55ab5a563feed766c28714a285edcf2a8bd9d01b
yqy <yqy@yyqyy.com>
2021-10-28 23:19:34 +0800

    2021/10/28 23:19

---
+++ forum-web/src/main/java/com/join/forumWeb/util/JsonUtil.java
@@ -0,0 +1,13 @@
+package com.join.forumWeb.util;
+
+import com.fasterxml.jackson.databind.json.JsonMapper;
+
+public class JsonUtil {
+
+    public static String packToJson(Object obj) {
+        JsonMapper mapper = new JsonMapper();
+
+        return "";
+    }
+
+}
